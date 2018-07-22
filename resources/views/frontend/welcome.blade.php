87836b4f9ed6a13ae09451843c43fe8d13f6deda
zlovegame5z <zlovegame5z@gmail.com>
2018-07-22 20:42:25 +0800

    Layout設定結束

--- resources/views/frontend/welcome.blade.php
+++ resources/views/frontend/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    你好啊 ! {{$name}} 。
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.welcome', ['name' => '大帥哥']);
 });
