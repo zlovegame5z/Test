f7739e0e0ae02752a12bebd5f8bbe61686050bb9
zlovegame5z <zlovegame5z@gmail.com>
2018-07-23 16:42:36 +0800

    Backend done

---
+++ app/Models/Home.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Home extends Model
+{
+    //
+    protected $table = 'home';
+    public $timestamps = false;
+}
---
+++ app/Models/Website.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Website extends Model
+{
+    //
+    protected $table = 'website';
+    public $timestamps = false;
+}
