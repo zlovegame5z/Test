ae3a832616f00dfa9ea585043be1fd0d4c7dbed5
zlovegame5z <zlovegame5z@gmail.com>
2018-07-23 13:30:21 +0800

    Database done

--- database/migrations/2018_07_23_051448_create_home_table.php
+++ database/migrations/2018_07_23_051448_create_home_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateHomeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('home', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->text('content_1'); //等於TEXT 欄位
+            $table->text('content_2');
+            $table->string('image');    //存image路徑
         });
     }
 
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('home');
     }
 }
